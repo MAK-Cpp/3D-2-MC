0e107e6fe9ea988dd2dea7c59adde00219664f92
MAK-Cpp <spartmenik@gmail.com>
2023-09-13 22:35:13 +0300

    clang-formatted

--- lib/figures/cube.cpp
+++ lib/figures/cube.cpp
@@ -3,10 +3,7 @@
 static constexpr glm::mat4 kIdentityMatrix(1);
 
 figure::Cube::Cube(const float x, const float y, const float z)
-    : model_(glm::translate(kIdentityMatrix, glm::vec3(x, y, z))) 
-    , X(model_[3][0])
-    , Y(model_[3][1])
-    , Z(model_[3][2]) {
+    : model_(glm::translate(kIdentityMatrix, glm::vec3(x, y, z))), X(model_[3][0]), Y(model_[3][1]), Z(model_[3][2]) {
     glGenVertexArrays(1, &VertexArrayID_);
     glBindVertexArray(VertexArrayID_);
 
@@ -22,10 +19,7 @@ figure::Cube::Cube(const float x, const float y, const float z)
 }
 
 figure::Cube::Cube(const glm::vec3& xyz)
-    : model_(glm::translate(kIdentityMatrix, xyz))
-    , X(model_[3][0])
-    , Y(model_[3][1])
-    , Z(model_[3][2]) {
+    : model_(glm::translate(kIdentityMatrix, xyz)), X(model_[3][0]), Y(model_[3][1]), Z(model_[3][2]) {
     glGenVertexArrays(1, &VertexArrayID_);
     glBindVertexArray(VertexArrayID_);
 
@@ -43,10 +37,7 @@ figure::Cube::Cube(const glm::vec3& xyz)
 }
 
 figure::Cube::Cube(const figure::Cube& other)
-    : model_(other.model_)
-    , X(model_[3][0])
-    , Y(model_[3][1])
-    , Z(model_[3][2]) {
+    : model_(other.model_), X(model_[3][0]), Y(model_[3][1]), Z(model_[3][2]) {
     glGenVertexArrays(1, &VertexArrayID_);
     glBindVertexArray(VertexArrayID_);
 
@@ -61,7 +52,6 @@ figure::Cube::Cube(const figure::Cube& other)
     glGenBuffers(1, &colorbuffer_);
     glBindBuffer(GL_ARRAY_BUFFER, colorbuffer_);
     glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_DYNAMIC_DRAW);
-
 }
 
 figure::Cube::~Cube() {
@@ -70,12 +60,11 @@ figure::Cube::~Cube() {
     glDeleteBuffers(1, &colorbuffer_);
 }
 
-
 figure::Cube& figure::Cube::operator=(const figure::Cube& other) {
     model_ = other.model_;
-    X = model_[3][0];
-    Y = model_[3][1];
-    Z = model_[3][2];
+    X      = model_[3][0];
+    Y      = model_[3][1];
+    Z      = model_[3][2];
 
     return *this;
 }
@@ -89,30 +78,29 @@ void figure::Cube::Draw() const {
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer_);
     glVertexAttribPointer(0,  // attribute 0. No particular reason for 0,
-                            // but must match the layout in the shader.
-                        3,         // size
-                        GL_FLOAT,  // type
-                        GL_FALSE,  // normalized?
-                        0,         // stride
-                        (void*)0   // array buffer offset
+                              // but must match the layout in the shader.
+                          3,         // size
+                          GL_FLOAT,  // type
+                          GL_FALSE,  // normalized?
+                          0,         // stride
+                          (void*)0   // array buffer offset
     );
 
     // 2nd attribute buffer : colors
     glEnableVertexAttribArray(1);
     glBindBuffer(GL_ARRAY_BUFFER, colorbuffer_);
     glVertexAttribPointer(1,  // attribute. No particular reason for 1, but
-                            // must match the layout in the shader.
-                        3,         // size
-                        GL_FLOAT,  // type
-                        GL_FALSE,  // normalized?
-                        0,         // stride
-                        (void*)0   // array buffer offset
+                              // must match the layout in the shader.
+                          3,         // size
+                          GL_FLOAT,  // type
+                          GL_FALSE,  // normalized?
+                          0,         // stride
+                          (void*)0   // array buffer offset
     );
 
     // Draw the triangle !
-    glDrawArrays(
-        GL_TRIANGLES, 0,
-        3 * 12);  // Starting from vertex 0; 3 vertices total -> 1 triangle
+    glDrawArrays(GL_TRIANGLES, 0,
+                 3 * 12);  // Starting from vertex 0; 3 vertices total -> 1 triangle
     glDisableVertexAttribArray(0);
     glDisableVertexAttribArray(1);
 }
